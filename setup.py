cb1cd1e5097a25df1b37ecf84b3055fab8c457a1
Anton Lvov <ant.lvov@gmail.com>
2017-12-18 02:07:11 +0500

    README in reStructured text format for PyPi description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 
 # Get the long description from the readme file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 
